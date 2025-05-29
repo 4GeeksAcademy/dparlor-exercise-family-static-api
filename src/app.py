8850fd0804066fac0fd09ed42e5d2706712e8632
Dabeldab <1dareios@gmail.com>
2025-05-29 00:14:35 +0000

    save

--- src/app.py
+++ src/app.py
@@ -15,6 +15,35 @@ CORS(app)
 
 # Create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+member1 = {
+        "first_name": "John",
+        "last_name": "Jackson",
+        "age": 33,
+        "lucky_numbers": [7, 13, 22]
+}
+
+member2 = {
+    "first_name": "Jane",
+    "last_name": "Jackson",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+member3 = {
+    "first_name": "Jimmy",
+    "last_name": "Jackson",
+    "age": 5,
+    "lucky_numbers": 1
+}
+member4 = {
+    "first_name": "Michael",
+    "last_name": "Jackson",
+    "age": 33,
+    "lucky_numbers": [22, 33, 45, 17]
+}
+jackson_family.add_member(member1)
+jackson_family.add_member(member2)
+jackson_family.add_member(member3)
 
 
 # Handle/serialize errors like a JSON object
@@ -28,16 +57,27 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
+#ALL members returned
 @app.route('/members', methods=['GET'])
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
+    response_body = members
     return jsonify(response_body), 200
 
+#single member return
+@app.route('/members/<int:id>', methods=['GET'])
+def get_single(id):
+    single = jackson_family.get_member(id)
+    return single,200
 
+#@app.route('/members', methods=['POST'])
+def add_member():
+    member = request.json
+    jackson_family.add_member(member)
+    print(jackson_family.get_all_members())
+    return "Success!!", 200
+    
 
 # This only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
